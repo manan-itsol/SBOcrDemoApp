874d256cdd47d88d23ff5385b8c1fd3cd2335f01
manan-tkxel <abdul.mannan@tkxel.com>
2022-02-14 14:12:05 +0500

    Add project files.

---
+++ SBOcrDemoApp/SBOcrDemoApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SBOcrDemoApp.ViewModels;
+using SBOcrDemoApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SBOcrDemoApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ SBOcrDemoApp/SBOcrDemoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SBOcrDemoApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SBOcrDemoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

d1937f46ea3e4836bb8b86a1b61d07db4ae4bdac
manan-itsol <abdulmannan9669@gmail.com>
2022-03-14 22:56:19 +0500

    OCR implementations

--- SBOcrDemoApp/SBOcrDemoApp.Android/MainActivity.cs
+++ SBOcrDemoApp/SBOcrDemoApp.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using ScanbotSDK.Xamarin.Forms;
 
 namespace SBOcrDemoApp.Droid
 {
@@ -16,6 +17,26 @@ namespace SBOcrDemoApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            const string LICENSE_KEY =
+  "A3DQdaSlrCotN4ctoEjTRUAkagqjOr" +
+  "uuHkipFnL1LtPpAiazzGrTqaXB2IJA" +
+  "ZNLo2R1jytf82ENYlLuAFvcvh5WtCQ" +
+  "h9VY9uyDKm+bMtbPvEDpPCYmqWDQ6F" +
+  "QbCRqPoej2L5bE29yDhBqwkrz9JBxH" +
+  "ogUGhOtjxAuLu/qWsCWd/1OXsIi2Jy" +
+  "U1Uw1dlHrq4AzLflKrJxRd9hjqCdvE" +
+  "7x7m/psi1UohfgVmN7adQk+h84D901" +
+  "Fr/jRQFRsxxGxvZPi8JpII2301cdre" +
+  "C+vqxGT2nuB7CC857OlRA5OddC8wwe" +
+  "6oVj+vZujNDR0nVRpW2EEPEEPAvLzS" +
+  "X0hEc52SjQOA==\nU2NhbmJvdFNESw" +
+  "pjb20uY29tcGFueW5hbWUuc2JvY3Jk" +
+  "ZW1vYXBwCjE2NDU0ODc5OTkKODM4OD" +
+  "YwNwoz\n";
+            // You can pass null as licenseKey for trial mode. See the "License" section for more details.
+            SBSDKInitializer.Initialize(Application, LICENSE_KEY, new SBSDKConfiguration { EnableLogging = true });
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- SBOcrDemoApp/SBOcrDemoApp/ViewModels/AboutViewModel.cs
+++ SBOcrDemoApp/SBOcrDemoApp/ViewModels/AboutViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ScanbotSDK.Xamarin.Forms;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -11,8 +14,58 @@ namespace SBOcrDemoApp.ViewModels
         {
             Title = "About";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            CaptureCommand = new Command(async () => await ExecuteCaptureCommand());
         }
 
         public ICommand OpenWebCommand { get; }
+        public ICommand CaptureCommand { get; }
+
+        private string _extractedText;
+        public string ExtractedText
+        {
+            get
+            {
+                return _extractedText;
+            }
+            set
+            {
+                _extractedText = value;
+                OnPropertyChanged(nameof(ExtractedText));
+            }
+        }
+
+        private async Task ExecuteCaptureCommand()
+        {
+            var config = new DocumentScannerConfiguration();
+            var result = await SBSDK.UI.LaunchDocumentScannerAsync(config);
+            if (result.Status == OperationResult.Ok)
+            {
+                var image = result.Pages[0].Document;
+
+
+                var languages = new[] { "en", "la" }; // or specify more languages like { "en", "de", ... }
+                var ocrResult = await SBSDK.Operations.PerformOcrAsync(result.Pages.Select(x => x.Document), languages);
+
+                ExtractedText = ocrResult.Text;
+                // See the recognized plain text of all images/pages via result.Text,
+                // or get more details of each image/page via result.Pages:
+                foreach (var ocrPage in ocrResult.Pages)
+                {
+                    //ocrPage.Paragraphs ...
+                    //ocrPage.Lines ...
+                    //ocrPage.Words ...
+                    // example for words:
+                    foreach (var wordBlock in ocrPage.Words)
+                    {
+                        //wordBlock.Text ...
+                        //wordBlock.BoundingBox.X ...
+                        //wordBlock.BoundingBox.Y ...
+                        //wordBlock.BoundingBox.Width ...
+                        //wordBlock.BoundingBox.Height ...
+                    }
+                }
+
+            }
+        }
     }
 }
